f817a1b1de39645e29c1ab957716c86c4f8e41f0
Leslie Williams <Seslie2009@yahoo.com>
2021-06-17 20:44:04 -0700

    trying the email stuff

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,11 +1,58 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import flutepic from '../images/flute.jpg';   
+import flutepic from '../images/flute.jpg';  
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
+import emailjs from 'emailjs-com';
+require('dotenv').config();
 function Contact(props) {
-    
+    const [name, setName] = useState();
+    const [email, setEmail] = useState();
+    const [subject, setSubject] = useState();
+    const [message, setMessage] = useState();
+
+
+
+    const toastifySuccess = () => {
+        toast('Email sent!', {
+          position: 'bottom-right',
+          autoClose: 5000,
+          hideProgressBar: true,
+          closeOnClick: true,
+          pauseOnHover: true,  
+          draggable: false,
+          className: 'submit-feedback success',
+          toastId: 'notifyToast'
+        });
+      };
+
+      const onSubmit = async (data) => {
+        try {
+          const templateParams = {
+            name: data.name,
+            email: data.email,
+            subject: data.subject,
+            message: data.message
+          };
+          await emailjs.send(
+            process.env.SERVICE_ID,
+            process.env.TEMPLATE_ID,
+            templateParams,
+            process.env.USER_ID
+          );
+          setEmail('');
+          setName('');
+          setSubject('');
+          setMessage('');
+          toastifySuccess();
+        } catch (e) {
+          console.log(e);
+        }
+      };
     return (
         <>
             <Container style={{backgroundImage: `url(${flutepic})` , backgroundSize: 'cover', backgroundRepeat: 'no-repeat', width:'100vw', height: '100vh', maxWidth: '100vw'}}>
@@ -34,6 +81,7 @@ function Contact(props) {
                                 </Form.Group>
                             </Row>
                         </Form>
+                        <ToastContainer/>
                     </Col>
                     <Col md={2}></Col>
                 </Row>
