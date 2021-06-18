55dbe2a404abee5f153b31e4d2610774e714c20f
Leslie Williams <Seslie2009@yahoo.com>
2021-06-17 22:27:13 -0700

    email working develop works, just need to change my routes

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,6 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 import flutepic from '../images/flute.jpg';  
 
 import { ToastContainer, toast } from 'react-toastify';
@@ -30,20 +31,23 @@ function Contact(props) {
         });
       };
 
-      const onSubmit = async (data) => {
+      const onSubmit = async (event) => {
+          event.preventDefault();
         try {
           const templateParams = {
-            name: data.name,
-            email: data.email,
-            subject: data.subject,
-            message: data.message
+            name: name,
+            email: email,
+            subject: subject,
+            message: message
           };
+          console.log(process.env.REACT_APP_SERVICE_ID);
           await emailjs.send(
-            process.env.SERVICE_ID,
-            process.env.TEMPLATE_ID,
+              //react needs .env variable to start with REACT_APP_
+            process.env.REACT_APP_SERVICE_ID,
+            process.env.REACT_APP_TEMPLATE_ID,
             templateParams,
-            process.env.USER_ID
-          );
+            process.env.REACT_APP_USER_ID
+          )
           setEmail('');
           setName('');
           setSubject('');
@@ -59,27 +63,34 @@ function Contact(props) {
                 <Row style={{paddingTop: "2rem"}}>
                     <Col md={2}></Col>
                     <Col>
-                        <Form style={{backgroundColor: 'rgb(148,139,140, .7)', padding:'1rem 1.5rem 1rem 1.5rem'}}>
+                        <Form style={{backgroundColor: 'rgb(148,139,140, .7)', padding:'1rem 1.5rem 1rem 1.5rem'}} onSubmit={ e=>onSubmit(e)}>
                             <Row>
                                 <Col>   
                                     <Form.Group className="mb-3" controlId="FormName">
                                         <Form.Label style={{fontFamily: 'Marck Script, cursive', fontWeight: 'bold'}}>Name</Form.Label>
-                                        <Form.Control type="email" placeholder="Name" />
+                                        <Form.Control type="text" placeholder="Name" onChange={event => setName(event.target.value.trim())} />
                                     </Form.Group>
                                 </Col>
                                 <Col>
                                     <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                                         <Form.Label style={{fontFamily: 'Marck Script, cursive', fontWeight: 'bold'}}>Email address</Form.Label>
-                                        <Form.Control type="email" placeholder="name@email.com"/>
+                                        <Form.Control type="email" placeholder="name@email.com" onChange={event => setEmail(event.target.value.trim())}/>
                                     </Form.Group>
                                 </Col> 
                             </Row>
                             <Row>
+                            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                                        <Form.Label style={{fontFamily: 'Marck Script, cursive', fontWeight: 'bold'}}>Subject</Form.Label>
+                                        <Form.Control type="text" placeholder="Subject" onChange={event => setSubject(event.target.value.trim())}/>
+                                    </Form.Group>
                                 <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
                                     <Form.Label style={{fontFamily: 'Marck Script, cursive', fontWeight: 'bold'}}>Message</Form.Label>
-                                    <Form.Control as="textarea" rows={3} />
+                                    <Form.Control as="textarea" rows={3} onChange={event => setMessage(event.target.value.trim())}/>
                                 </Form.Group>
                             </Row>
+                            <Button variant="primary" type="submit">
+                                Send
+                            </Button>
                         </Form>
                         <ToastContainer/>
                     </Col>
