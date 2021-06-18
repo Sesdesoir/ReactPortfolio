ceac46d0510409e40ed6bab4d60366f3ef3545b3
Leslie Williams <Seslie2009@yahoo.com>
2021-06-17 23:09:52 -0700

    hash should work on gh pages now

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,13 +6,13 @@ function Header(props) {
     return (
         <>
             <Navbar bg="light" expand="lg" style={{background: `linear-gradient(to right, #A587CA, #36CEDC, #8FE968, #FFEA56, #FFB750, #FE797B)`}}>
-                <Navbar.Brand href="/" style={{fontFamily: 'Marck Script, cursive', fontWeight: '570'}}>Leslie Williams</Navbar.Brand>
+                <Navbar.Brand href="/react-portfolio/#/" style={{fontFamily: 'Marck Script, cursive', fontWeight: '570'}}>Leslie Williams</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
-                        <Nav.Link href="/#/" style={{fontFamily: 'Marck Script, cursive', fontWeight: '570'}}>About Me</Nav.Link>
-                        <Nav.Link href="/#/contact" style={{fontFamily: 'Marck Script, cursive', fontWeight: '570'}}>Contact</Nav.Link>
-                        <Nav.Link href="/#/projects" style={{fontFamily: 'Marck Script, cursive', fontWeight: '570'}}>Projects</Nav.Link>
+                        <Nav.Link href="/react-portfolio/#/" style={{fontFamily: 'Marck Script, cursive', fontWeight: '570'}}>About Me</Nav.Link>
+                        <Nav.Link href="/react-portfolio/#/" style={{fontFamily: 'Marck Script, cursive', fontWeight: '570'}}>Contact</Nav.Link>
+                        <Nav.Link href="/react-portfolio/#/" style={{fontFamily: 'Marck Script, cursive', fontWeight: '570'}}>Projects</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
